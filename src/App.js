21de1c42dd6b1d84d2a11e1d4f46cbc93e340dae
guillermo heredia <guillhere76@gmail.com>
2022-06-06 21:03:00 -0300

    Se agrego react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Editorial GDM
+        </h1>
+        <p>Aguarde un momento, estamos programando.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
