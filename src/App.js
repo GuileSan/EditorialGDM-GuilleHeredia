244c597fd668fd380034e2076164a6959872c192
guillermo heredia <guillhere76@gmail.com>
2022-06-13 20:07:10 -0300

    Anadido Nav Bar a la app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+//@ts-check
 
+import './App.css';
+import Navbar from './components/NavBar/Navbar';
 function App() {
+
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
+        
         <h1>
           Editorial GDM
-        </h1>
+      </h1>
         <p>Aguarde un momento, estamos programando.</p>
       </header>
     </div>
