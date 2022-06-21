f40c1a0a6e86163cabc86b80985b89731ea87045
guillermo heredia <guillhere76@gmail.com>
2022-06-20 22:27:46 -0300

    Se agrego componente ItemCount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 //@ts-check
 
 import './App.css';
+import ItemCount from './components/itemCount/ItemCount';
 import ItemListContainer from './components/itemsListContainer/ItemListContainer';
 import Navbar from './components/NavBar/Navbar';
 function App() {
+  let initial = 1;
+  let stock = 8;
 
+  function onAdd(valor) {
+    alert("Agregaste" + valor + "productos a tu compra")
+  }
   return (
     <div className="App">
       <Navbar></Navbar>
       <header className="App-header">
         <ItemListContainer greeting="Bienvenido al sitio oficial de Editorial GDM" />
+       
       </header>
+      <body>
+         <ItemCount initial ={initial} stock ={stock} onAdd ={onAdd}/>
+      </body>
     </div>
   );
 }
---
+++ src/components/itemCount/ItemCount.jsx
@@ -0,0 +1,32 @@
+import React, { useState} from 'react'
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import "./ItemCount.css";
+
+export default function ItemCount ({initial, stock, onAdd}) {
+
+    const [count, setCount] = useState(initial);
+
+    function restar() {
+        if(count != 1){
+            setCount(count -1);
+        }
+    }
+    function sumar() {
+        if (count != stock) {
+            setCount(count + 1);
+        }
+    }
+
+  return (
+    <div className="counter">
+        <div className="cantidad">{count}</div>
+        <div className="botones">
+            <div><Button variant="contained" onClick ={() => restar()}>-</Button></div>
+            <div><Button variant="contained" onClick ={() => sumar()}>+</Button></div>
+            <div><Button variant="contained" onClick ={() => onAdd(count)}>Add to Cart</Button></div>
+        </div>
+      
+    </div>
+  )
+}
